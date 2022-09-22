aa723bdd18d4d6a86d0556f0d8bc1fdae0a81bf3
sanikumar <sanikumarsahani@gmail.com>
2022-09-22 21:39:52 +0530

    navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,51 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ onSearch, cartItemCount }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchQuery);
+    if (searchQuery.trim().length) {
+      onSearch(searchQuery.trim());
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello NavBar</h1>
+    <div className="wrapper">
+      <header className="container">
+        <div className="header py-2">
+          <div className="grid">
+            <Link to="/" className="link">
+              <h1 className="brand">E-store</h1>
+            </Link>
+            <div className="formContainer">
+              <form className="search">
+                <div className="form-control">
+                  <input
+                    type="text"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    placeholder="search products..."
+                  />
+                </div>
+                <button type="submit" onClick={handleSubmit} className="search-btn">
+                  search
+                </button>
+              </form>
+            </div>
+            <Link to="/cart" className="link headerCart">
+              <img className="cartImg" src="/cart.svg" />
+              {cartItemCount > 0 && (
+                <div className="cartCounter">
+                  {cartItemCount <= 9 ? cartItemCount : "9+"}
+                </div>
+              )}
+            </Link>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
